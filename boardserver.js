98456ebd35c501b9839fb34b8329012521750ead
Dann Luciano <dannluciano@gmail.com>
2020-04-27 13:07:42 -0300

    Add Compression on responses

--- boardserver.js
+++ boardserver.js
@@ -1,8 +1,10 @@
 const express = require('express')
+const compression = require('compression')
 var app = express();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
+app.use(compression())
 app.use(express.static('public'));
 
 app.get('/hello', (req, res) => {
